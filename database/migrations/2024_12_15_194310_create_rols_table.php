757244b5e6763af12225cbc99eeddb1d6f60f3a9
Luis Calle Lazo <callelazodeynarluis@gmail.com>
2024-12-15 16:32:42 -0400

    Nigration

--- database/migrations/2024_12_15_194310_create_rols_table.php
+++ database/migrations/2024_12_15_194310_create_rols_table.php
@@ -11,8 +11,46 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('user_info', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->constrained()->onDelete('cascade');
+            $table->string('country');
+            $table->integer('code');
+            $table->integer('phone');
+            $table->string('email');
+            $table->string('photo');
+            $table->timestamps();
+        });
+
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('lawyer', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
         Schema::create('rols', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
+        });
+
+        Schema::create('admin_rol', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('admin_id')->constrained('admin')->onDelete('cascade');
+            $table->foreignId('rol_id')->constrained('rols')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -22,6 +60,11 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('admin');
+        Schema::dropIfExists('user_info');
+        Schema::dropIfExists('client');
+        Schema::dropIfExists('lawyer');
         Schema::dropIfExists('rols');
+        Schema::dropIfExists('admin_rol');
     }
 };

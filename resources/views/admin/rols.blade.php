74d1b4a3e6a46877f24a5b1d746cfd0f6476996a
Luis Calle Lazo <callelazodeynarluis@gmail.com>
2024-12-16 12:23:08 -0400

    Rols Salvada update

--- resources/views/admin/rols.blade.php
+++ resources/views/admin/rols.blade.php
@@ -11,7 +11,7 @@
     })->toArray();
 
     $buttons = [
-        ['bi bi-pencil-square', '#278', '#fff', 'EditUserModal'],
+        ['bi bi-pencil-square', '#278', '#fff', 'EditRolModal'],
     ];
 @endphp
 
@@ -32,5 +32,61 @@
                 :pageNumber="$pageNumber" link="admins" :totalPages="$totalPages" />
         </div>
     </div>
+
+    {{-- EDITAR --}}
+    <x-modal width="400px" height="300px" title="Editar nombre del rol" idModal="editModal" idCloseModal="closeEditModal">
+
+        <div class="flex justify-center items-center flex-col">
+            <h3 class="py-4">Nombre de rol:</h3>
+            <x-input-text placeholder="Nombre de rol" id="name_rol_up" width="280px" />
+        </div>
+        <x-slot name="btn_action">
+            <x-button-text id="btnEditUser" color="#fff" bg="#007bff" text="Editar" function="EditRol"/>
+        </x-slot>
+    </x-modal>
+
+    <script>
+        function EditRolModal(param)
+        {
+            let array = JSON.parse(param);
+            localStorage.setItem("idRolSystem", array[0])
+
+            document.getElementById("name_rol_up").value = array[1];
+
+            const modal = new ModalPrefab("editModal", "closeEditModal");
+            modal.openModal();
+        }
+
+        async function EditRol() {
+            const idRol = localStorage.getItem("idRolSystem");
+            const name = document.getElementById("name_rol_up").value;
+
+            const axiosInstance = new AxiosInstance();
+            axiosInstance.showSuccessMsg("Se actualizo correctamente");
+            axiosInstance.hideAllModals();
+        }
+    </script>
+
+
+    {{-- FILTER --}}
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const searchInput = document.getElementById('filter');
+            const rows = document.querySelectorAll('tbody tr');
+
+            searchInput.addEventListener('keyup', function () {
+                const searchTerm = this.value.trim().toLowerCase();
+
+                rows.forEach(row => {
+                    const name = row.querySelector('td:nth-child(1)').textContent.trim().toLowerCase();
+                    if (name.includes(searchTerm)) {
+                        row.style.display = '';
+                    } else {
+                        row.style.display = 'none';
+                    }
+                });
+            });
+        });
+    </script>
 </div>
 @endsection
